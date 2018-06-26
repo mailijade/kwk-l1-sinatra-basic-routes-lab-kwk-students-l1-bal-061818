5ef7aa80ea90b58afe7f9f6bb742ec2b8313f6aa
mailijade <mailijade23@gmail.com>
2018-06-26 16:03:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "My name is Mai li"
+  end
+  get '/' do
+  "My hometown is New York"
+  end
+  get '/' do 
+    "My favorite song is Say Amen, by P!ATD"
+  end
 end
